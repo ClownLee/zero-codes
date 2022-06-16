07d44fe1d845c10941fa6b08bc137e6ef92cef14
ele_11_ven <lwentao@hotmail.com>
2022-06-16 06:20:16 +0000

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { ZeroCodesSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  ZeroCodesSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <zero-codes-sample />
+  </div>
+</template>
